3d141384821fe3fdc10fe03c8d078d6465ed86f5
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2024-08-04 14:56:17 -0300

    bugfix:core.business.repositories.content

--- sm-core/src/main/java/com/salesmanager/core/business/repositories/content/ContentRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/content/ContentRepository.java
@@ -7,39 +7,40 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.salesmanager.core.model.content.Content;
 import com.salesmanager.core.model.content.ContentType;
+import org.springframework.data.repository.query.Param;
 
 public interface ContentRepository extends JpaRepository<Content, Long>,  ContentRepositoryCustom  {
 
 	
-	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.contentType = ?1 and cm.id = ?2 and cd.language.id = ?3 order by c.sortOrder asc")
-	List<Content> findByType(ContentType contentType, Integer storeId, Integer languageId);
+	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.contentType = :contentType and cm.id = :storeId and cd.language.id = :languageId order by c.sortOrder asc")
+	List<Content> findByType(@Param("contentType") ContentType contentType, @Param("storeId") Integer storeId, @Param("languageId") Integer languageId);
 	
-	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.contentType = ?1 and cm.id = ?2 order by c.sortOrder asc")
-	List<Content> findByType(ContentType contentType, Integer storeId);
+	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.contentType = :contentType and cm.id = :storeId order by c.sortOrder asc")
+	List<Content> findByType(@Param("contentType") ContentType contentType, @Param("storeId") Integer storeId);
 	
-	@Query("select c from Content c join fetch c.merchantStore cm where c.code = ?1 and c.contentType = ?2 and cm.id = ?3")
-	Content findByCodeAndType(String code, ContentType contentType, Integer storeId);
+	@Query("select c from Content c join fetch c.merchantStore cm where c.code = :code and c.contentType = :contentType and cm.id = :storeId")
+	Content findByCodeAndType(@Param("code") String code, @Param("contentType") ContentType contentType, @Param("storeId") Integer storeId);
 	
-	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.contentType in (?1) and cm.id = ?2 and cd.language.id = ?3 order by c.sortOrder asc")
-	List<Content> findByTypes(List<ContentType> contentTypes, Integer storeId, Integer languageId);
+	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.contentType in (:contentTypes) and cm.id = :storeId and cd.language.id = :languageId order by c.sortOrder asc")
+	List<Content> findByTypes(@Param("contentTypes") List<ContentType> contentTypes, @Param("storeId") Integer storeId, @Param("languageId") Integer languageId);
 	
-	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.contentType in (?1) and cm.id = ?2 order by c.sortOrder asc")
-	List<Content> findByTypes(List<ContentType> contentTypes, Integer storeId);
+	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.contentType in (:contentTypes) and cm.id = :storeId order by c.sortOrder asc")
+	List<Content> findByTypes(@Param("contentTypes") List<ContentType> contentTypes, @Param("storeId") Integer storeId);
 	
-	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.code = ?1 and cm.id = ?2")
-	Content findByCode(String code, Integer storeId);
+	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.code = :code and cm.id = :storeId")
+	Content findByCode(@Param("code") String code, @Param("storeId") Integer storeId);
 	
-	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.contentType = ?1 and cm.id=?3 and c.code like ?2 and cd.language.id = ?4")
-	List<Content> findByCodeLike(ContentType contentType, String code, Integer storeId, Integer languageId);
+	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.contentType = :contentType and cm.id=:storeId and c.code like :code and cd.language.id = :languageId")
+	List<Content> findByCodeLike(@Param("contentType") ContentType contentType, @Param("code") String code, @Param("storeId") Integer storeId, @Param("languageId") Integer languageId);
 	
-	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.code = ?1 and cm.id = ?2 and cd.language.id = ?3")
-	Content findByCode(String code, Integer storeId, Integer languageId);
+	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.code = :code and cm.id = :storeId and cd.language.id = :languageId")
+	Content findByCode(@Param("code") String code, @Param("storeId") Integer storeId, @Param("languageId") Integer languageId);
 	
-	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.id = ?1 and cd.language.id = ?2")
-	Content findByIdAndLanguage(Long contentId, Integer languageId);
+	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.id = :contentId and cd.language.id = :languageId")
+	Content findByIdAndLanguage(@Param("contentId") Long contentId, @Param("languageId") Integer languageId);
 	
-	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.id = ?1")
-	Content findOne(Long contentId);
+	@Query("select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.id = :contentId")
+	Content findOne(@Param("contentId") Long contentId);
 
 
 }
--- sm-core/src/main/java/com/salesmanager/core/business/repositories/content/PageContentRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/content/PageContentRepository.java
@@ -8,18 +8,19 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import com.salesmanager.core.model.content.Content;
 import com.salesmanager.core.model.content.ContentType;
 import com.salesmanager.core.model.merchant.MerchantStore;
+import org.springframework.data.repository.query.Param;
 
 public interface PageContentRepository extends PagingAndSortingRepository<MerchantStore, Long> {
 	
 	
 
-	@Query(value = "select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.contentType = ?1 and cm.id = ?2 order by c.sortOrder asc",
-      countQuery = "select count(distinct c) from Content c join c.merchantStore cm where c.contentType = ?1 and cm.id = ?2")
-	Page<Content> findByContentType(ContentType contentType, Integer storeId, Pageable pageable);
+	@Query(value = "select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm where c.contentType = :contentType and cm.id = :storeId order by c.sortOrder asc",
+      countQuery = "select count(distinct c) from Content c join c.merchantStore cm where c.contentType = :contentType and cm.id = :storeId")
+	Page<Content> findByContentType(@Param("contentType") ContentType contentType, @Param("storeId") Integer storeId, Pageable pageable);
 	
-	@Query(value = "select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm join fetch cd.language cdl where c.contentType = ?1 and cm.id = ?2 and cdl.id = ?3 order by c.sortOrder asc",
-		      countQuery = "select count(distinct c) from Content c join c.merchantStore cm where c.contentType = ?1 and cm.id = ?2")
-			Page<Content> findByContentType(ContentType contentTypes, Integer storeId, Integer language, Pageable pageable);
+	@Query(value = "select c from Content c left join fetch c.descriptions cd join fetch c.merchantStore cm join fetch cd.language cdl where c.contentType = :contentTypes and cm.id = :storeId and cdl.id = :language order by c.sortOrder asc",
+		      countQuery = "select count(distinct c) from Content c join c.merchantStore cm where c.contentType = :contentTypes and cm.id = :storeId")
+			Page<Content> findByContentType(@Param("contentTypes") ContentType contentTypes, @Param("storeId") Integer storeId, @Param("language") Integer language, Pageable pageable);
 	
 	
 
