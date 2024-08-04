592883362977478893b0c938371d6dacf66023de
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2024-08-04 12:01:41 -0300

    core.business.repositories.tax.TaxRateRepository

--- sm-core/src/main/java/com/salesmanager/core/business/repositories/tax/TaxClassRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/tax/TaxClassRepository.java
@@ -6,17 +6,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.salesmanager.core.model.tax.taxclass.TaxClass;
+import org.springframework.data.repository.query.Param;
 
 public interface TaxClassRepository extends JpaRepository<TaxClass, Long> {
 	
-	@Query("select t from TaxClass t left join fetch t.merchantStore tm where tm.id=?1")
-	List<TaxClass> findByStore(Integer id);
+	@Query("select t from TaxClass t left join fetch t.merchantStore tm where tm.id=:id")
+	List<TaxClass> findByStore(@Param("id") Integer id);
 	
-	@Query("select t from TaxClass t left join fetch t.merchantStore tm where t.code=?1")
-	TaxClass findByCode(String code);
+	@Query("select t from TaxClass t left join fetch t.merchantStore tm where t.code=:code")
+	TaxClass findByCode(@Param("code") String code);
 	
-	@Query("select t from TaxClass t left join fetch t.merchantStore tm where tm.id=?1 and t.code=?2")
-	TaxClass findByStoreAndCode(Integer id, String code);
+	@Query("select t from TaxClass t left join fetch t.merchantStore tm where tm.id=:id and t.code=:code")
+	TaxClass findByStoreAndCode(@Param("id") Integer id, @Param("code") String code);
 
 
 }
--- sm-core/src/main/java/com/salesmanager/core/business/repositories/tax/TaxRateRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/tax/TaxRateRepository.java
@@ -6,29 +6,30 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.salesmanager.core.model.tax.taxrate.TaxRate;
+import org.springframework.data.repository.query.Param;
 
 public interface TaxRateRepository extends JpaRepository<TaxRate, Long> {
 
-	@Query("select t from TaxRate t join fetch t.taxClass join fetch t.merchantStore tm join fetch t.country left join fetch t.zone left join fetch t.descriptions left join t.parent where tm.id=?1 order by t.taxPriority asc")
-	List<TaxRate> findByStore(Integer id);
+	@Query("select t from TaxRate t join fetch t.taxClass join fetch t.merchantStore tm join fetch t.country left join fetch t.zone left join fetch t.descriptions left join t.parent where tm.id=:id order by t.taxPriority asc")
+	List<TaxRate> findByStore(@Param("id") Integer id);
 	
-	@Query("select t from TaxRate t join fetch t.taxClass join fetch t.merchantStore tm join fetch t.country left join fetch t.zone left join fetch t.descriptions td left join t.parent where tm.id=?1 and td.language.id=?2 order by t.taxPriority asc")
-	List<TaxRate> findByStoreAndLanguage(Integer id, Integer languageId);
+	@Query("select t from TaxRate t join fetch t.taxClass join fetch t.merchantStore tm join fetch t.country left join fetch t.zone left join fetch t.descriptions td left join t.parent where tm.id=:id and td.language.id=:languageId order by t.taxPriority asc")
+	List<TaxRate> findByStoreAndLanguage(@Param("id") Integer id, @Param("languageId") Integer languageId);
 	
-	@Query("select t from TaxRate t join fetch t.taxClass join fetch t.merchantStore tm join fetch t.country left join fetch t.zone left join fetch t.descriptions td left join t.parent where tm.id=?1 and t.code=?2")
-	TaxRate findByStoreAndCode(Integer id, String code);
+	@Query("select t from TaxRate t join fetch t.taxClass join fetch t.merchantStore tm join fetch t.country left join fetch t.zone left join fetch t.descriptions td left join t.parent where tm.id=:id and t.code=:code")
+	TaxRate findByStoreAndCode(@Param("id") Integer id, @Param("code") String code);
 	
-	@Query("select t from TaxRate t join fetch t.taxClass join fetch t.merchantStore tm join fetch t.country left join fetch t.zone left join fetch t.descriptions td left join t.parent where tm.id=?1 and t.id=?2")
-	TaxRate findByStoreAndId(Integer store, Long id);
+	@Query("select t from TaxRate t join fetch t.taxClass join fetch t.merchantStore tm join fetch t.country left join fetch t.zone left join fetch t.descriptions td left join t.parent where tm.id=:store and t.id=:id")
+	TaxRate findByStoreAndId(@Param("store") Integer store, @Param("id") Long id);
 	
-	@Query("select t from TaxRate t join fetch t.taxClass join fetch t.merchantStore tm join fetch t.country left join fetch t.zone left join fetch t.descriptions td left join t.parent where t.id=?1")
-	TaxRate findOne(Long id);
+	@Query("select t from TaxRate t join fetch t.taxClass join fetch t.merchantStore tm join fetch t.country left join fetch t.zone left join fetch t.descriptions td left join t.parent where t.id=:id")
+	TaxRate findOne(@Param("id") Long id);
 	
-	@Query("select t from TaxRate t join fetch t.taxClass join fetch t.merchantStore tm join fetch t.country tc left join fetch t.zone tz left join fetch t.descriptions td left join t.parent where tm.id=?1 AND (tz.id=?2 OR tz IS NULL) and tc.id=?3 and td.language.id=?4 order by t.taxPriority asc")
-	List<TaxRate> findByMerchantAndZoneAndCountryAndLanguage(Integer id, Long zoneId, Integer countryId, Integer languageId);
+	@Query("select t from TaxRate t join fetch t.taxClass join fetch t.merchantStore tm join fetch t.country tc left join fetch t.zone tz left join fetch t.descriptions td left join t.parent where tm.id=:id AND (tz.id=:zoneId OR tz IS NULL) and tc.id=:countryId and td.language.id=:languageId order by t.taxPriority asc")
+	List<TaxRate> findByMerchantAndZoneAndCountryAndLanguage(@Param("id") Integer id, @Param("zoneId") Long zoneId, @Param("countryId") Integer countryId, @Param("languageId") Integer languageId);
 	
-	@Query("select t from TaxRate t join fetch t.taxClass join fetch t.merchantStore tm join fetch t.country tc left join fetch t.zone tz left join fetch t.descriptions td left join t.parent where tm.id=?1 AND t.stateProvince=?2 and tc.id=?3 and td.language.id=?4 order by t.taxPriority asc")
-	List<TaxRate> findByMerchantAndProvinceAndCountryAndLanguage(Integer id, String province, Integer countryId, Integer languageId);
+	@Query("select t from TaxRate t join fetch t.taxClass join fetch t.merchantStore tm join fetch t.country tc left join fetch t.zone tz left join fetch t.descriptions td left join t.parent where tm.id=:id AND t.stateProvince=:province and tc.id=:countryId and td.language.id=:languageId order by t.taxPriority asc")
+	List<TaxRate> findByMerchantAndProvinceAndCountryAndLanguage(@Param("id") Integer id, @Param("province") String province, @Param("countryId") Integer countryId, @Param("languageId") Integer languageId);
 	
 	
 }
