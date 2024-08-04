6d8e6351454ac67b4e0214952c4d8ed2f1c52208
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2024-08-04 13:59:06 -0300

    bugfix:core.business.repositories.system

--- sm-core/src/main/java/com/salesmanager/core/business/repositories/system/MerchantConfigurationRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/system/MerchantConfigurationRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.salesmanager.core.model.system.MerchantConfiguration;
 import com.salesmanager.core.model.system.MerchantConfigurationType;
+import org.springframework.data.repository.query.Param;
 
 public interface MerchantConfigurationRepository extends JpaRepository<MerchantConfiguration, Long> {
 
@@ -14,12 +15,12 @@ public interface MerchantConfigurationRepository extends JpaRepository<MerchantC
 	
 	//MerchantConfiguration findByCode(String code);
 	
-	@Query("select m from MerchantConfiguration m join fetch m.merchantStore ms where ms.id=?1")
-	List<MerchantConfiguration> findByMerchantStore(Integer id);
+	@Query("select m from MerchantConfiguration m join fetch m.merchantStore ms where ms.id=:id")
+	List<MerchantConfiguration> findByMerchantStore(@Param("id") Integer id);
 	
-	@Query("select m from MerchantConfiguration m join fetch m.merchantStore ms where ms.id=?1 and m.key=?2")
-	MerchantConfiguration findByMerchantStoreAndKey(Integer id, String key);
+	@Query("select m from MerchantConfiguration m join fetch m.merchantStore ms where ms.id=:id and m.key=:key")
+	MerchantConfiguration findByMerchantStoreAndKey(@Param("id") Integer id, @Param("key") String key);
 	
-	@Query("select m from MerchantConfiguration m join fetch m.merchantStore ms where ms.id=?1 and m.merchantConfigurationType=?2")
-	List<MerchantConfiguration> findByMerchantStoreAndType(Integer id, MerchantConfigurationType type);
+	@Query("select m from MerchantConfiguration m join fetch m.merchantStore ms where ms.id=:id and m.merchantConfigurationType=:type")
+	List<MerchantConfiguration> findByMerchantStoreAndType(@Param("id") Integer id, @Param("type") MerchantConfigurationType type);
 }
--- sm-core/src/main/java/com/salesmanager/core/business/repositories/system/OptinRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/system/OptinRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.salesmanager.core.model.system.optin.Optin;
@@ -12,14 +13,14 @@ import com.salesmanager.core.model.system.optin.OptinType;
 @Repository
 public interface OptinRepository extends JpaRepository<Optin, Long> {
 
-	@Query("select distinct o from Optin as o  left join fetch o.merchant om where om.id = ?1")
-	List<Optin> findByMerchant(Integer storeId);
+	@Query("select distinct o from Optin as o  left join fetch o.merchant om where om.id = :storeId")
+	List<Optin> findByMerchant(@Param("storeId") Integer storeId);
 	
 
-	@Query("select distinct o from Optin as o  left join fetch o.merchant om where om.id = ?1 and o.optinType = ?2")
-	Optin findByMerchantAndType(Integer storeId, OptinType optinTyle);
+	@Query("select distinct o from Optin as o  left join fetch o.merchant om where om.id = :storeId and o.optinType = :optinTyle")
+	Optin findByMerchantAndType(@Param("storeId") Integer storeId, @Param("optinTyle") OptinType optinTyle);
 	
-	@Query("select distinct o from Optin as o  left join fetch o.merchant om where om.id = ?1 and o.code = ?2")
-	Optin findByMerchantAndCode(Integer storeId, String code);
+	@Query("select distinct o from Optin as o  left join fetch o.merchant om where om.id = :storeId and o.code = :code")
+	Optin findByMerchantAndCode(@Param("storeId") Integer storeId, @Param("code") String code);
 
 }
