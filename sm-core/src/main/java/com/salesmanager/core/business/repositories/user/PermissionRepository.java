3f2f9570450272069001bc8ea81e09e0649fe15e
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2024-08-04 11:48:08 -0300

    bugfix:core.business.repositories.user.PermissionRepository

--- sm-core/src/main/java/com/salesmanager/core/business/repositories/user/PermissionRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/user/PermissionRepository.java
@@ -7,16 +7,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.salesmanager.core.model.user.Permission;
+import org.springframework.data.repository.query.Param;
 
 public interface PermissionRepository extends JpaRepository<Permission, Integer>, PermissionRepositoryCustom {
 
 	
-	@Query("select p from Permission as p where p.id = ?1")
-	Permission findOne(Integer id);
+	@Query("select p from Permission as p where p.id = :id")
+	Permission findOne(@Param("id") Integer id);
 	
 	@Query("select p from Permission as p order by p.id")
 	List<Permission> findAll();
 	
-	@Query("select distinct p from Permission as p join fetch p.groups groups where groups.id in (?1)")
-	List<Permission> findByGroups(Set<Integer> groupIds);
+	@Query("select distinct p from Permission as p join fetch p.groups groups where groups.id in (:groupIds)")
+	List<Permission> findByGroups(@Param("groupIds") Set<Integer> groupIds);
 }
