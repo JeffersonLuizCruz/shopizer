d8cb15fb6a75ccb170475cb903ac8e580d8e4801
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2024-08-04 14:08:43 -0300

    bugifx:core.business.repositories.reference

--- sm-core/src/main/java/com/salesmanager/core/business/repositories/reference/country/CountryRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/reference/country/CountryRepository.java
@@ -6,22 +6,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.salesmanager.core.model.reference.country.Country;
+import org.springframework.data.repository.query.Param;
 
 
 public interface CountryRepository extends JpaRepository <Country, Integer> {
 	
-	@Query("select c from Country c left join fetch c.descriptions cd where c.isoCode=?1")
-	Country findByIsoCode(String code);
+	@Query("select c from Country c left join fetch c.descriptions cd where c.isoCode=:code")
+	Country findByIsoCode(@Param("code") String code);
 	
 
 	@Query("select c from Country c "
 			+ "left join fetch c.descriptions cd "
 			+ "left join fetch c.zones cz left join fetch cz.descriptions "
-			+ "where cd.language.id=?1")
-	List<Country> listByLanguage(Integer id);
+			+ "where cd.language.id=:id")
+	List<Country> listByLanguage(@Param("id") Integer id);
 	
 	/** get country including zones by language **/
-	@Query("select distinct c from Country c left join fetch c.descriptions cd left join fetch c.zones cz left join fetch cz.descriptions where cd.language.id=?1")
-	List<Country> listCountryZonesByLanguage(Integer id);
+	@Query("select distinct c from Country c left join fetch c.descriptions cd left join fetch c.zones cz left join fetch cz.descriptions where cd.language.id=:id")
+	List<Country> listCountryZonesByLanguage(@Param("id") Integer id);
 
 }
--- sm-core/src/main/java/com/salesmanager/core/business/repositories/reference/zone/ZoneRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/reference/zone/ZoneRepository.java
@@ -6,16 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.salesmanager.core.model.reference.zone.Zone;
+import org.springframework.data.repository.query.Param;
 
 public interface ZoneRepository extends JpaRepository<Zone, Long> {
 	
 	
 	Zone findByCode(String code);
 	
-	@Query("select z from Zone z left join fetch z.descriptions zd where zd.language.id=?1")
-	List<Zone> listByLanguage(Integer id);
+	@Query("select z from Zone z left join fetch z.descriptions zd where zd.language.id=:id")
+	List<Zone> listByLanguage(@Param("id") Integer id);
 	
-	@Query("select z from Zone z left join fetch z.descriptions zd join fetch z.country zc where zc.isoCode=?1 and zd.language.id=?2")
-	List<Zone> listByLanguageAndCountry(String isoCode, Integer languageId);
+	@Query("select z from Zone z left join fetch z.descriptions zd join fetch z.country zc where zc.isoCode=:isoCode and zd.language.id=:languageId")
+	List<Zone> listByLanguageAndCountry(@Param("isoCode") String isoCode, @Param("languageId") Integer languageId);
 
 }
