1a1b04321d4cfaedaf29f46de1464be2526bba69
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2024-08-13 22:16:49 -0300

    core.business.repositories.catalog.product.image

--- sm-core/src/main/java/com/salesmanager/core/business/repositories/catalog/product/image/ProductImageRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/catalog/product/image/ProductImageRepository.java
@@ -4,15 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.salesmanager.core.model.catalog.product.image.ProductImage;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
 
 
-	@Query("select p from ProductImage p left join fetch p.descriptions pd inner join fetch p.product pp inner join fetch pp.merchantStore ppm where p.id = ?1")
-	ProductImage findOne(Long id);
+	@Query("select p from ProductImage p left join fetch p.descriptions pd inner join fetch p.product pp inner join fetch pp.merchantStore ppm where p.id = :id")
+	ProductImage findOne(@Param("id") Long id);
 	
-	@Query("select p from ProductImage p left join fetch p.descriptions pd inner join fetch p.product pp inner join fetch pp.merchantStore ppm where pp.id = ?2 and ppm.code = ?3 and p.id = ?1")
-	ProductImage finById(Long imageId, Long productId, String storeCode);
+	@Query("select p from ProductImage p left join fetch p.descriptions pd inner join fetch p.product pp inner join fetch pp.merchantStore ppm where pp.id = :productId and ppm.code = :storeCode and p.id = :imageId")
+	ProductImage finById(@Param("imageId") Long imageId, @Param("productId") Long productId, @Param("storeCode") String storeCode);
 	
 	
 }
