9c28a61db57ba12cbff7a6d15f6a38f425f61ad2
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2024-08-17 00:11:14 -0300

    core.business.repositories.catalog.product.type

--- sm-core/src/main/java/com/salesmanager/core/business/repositories/catalog/product/type/PageableProductTypeRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/catalog/product/type/PageableProductTypeRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.salesmanager.core.model.catalog.product.type.ProductType;
+import org.springframework.data.repository.query.Param;
 
 public interface PageableProductTypeRepository extends PagingAndSortingRepository<ProductType, Long> {
 	
 	
-	@Query(value = "select distinct p from ProductType p left join fetch p.descriptions pd left join fetch p.merchantStore pm where pm.id=?1",
-		 countQuery = "select count(p) from ProductType p left join p.merchantStore pm where pm.id = ?1")
-	Page<ProductType> listByStore(Integer storeId, Pageable pageable);
+	@Query(value = "select distinct p from ProductType p left join fetch p.descriptions pd left join fetch p.merchantStore pm where pm.id=:storeId",
+		 countQuery = "select count(p) from ProductType p left join p.merchantStore pm where pm.id = :storeId")
+	Page<ProductType> listByStore(@Param("storeId") Integer storeId, Pageable pageable);
 
 }
