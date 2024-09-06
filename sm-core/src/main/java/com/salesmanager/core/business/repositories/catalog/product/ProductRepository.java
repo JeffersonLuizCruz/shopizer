7624f145b015fbbeff408aadbeacb0f0b5a13680
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2024-09-05 22:40:13 -0300

    core.business.repositories.catalog.product

--- sm-core/src/main/java/com/salesmanager/core/business/repositories/catalog/product/ProductRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/catalog/product/ProductRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.salesmanager.core.model.catalog.product.Product;
+import org.springframework.data.repository.query.Param;
 
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
@@ -15,16 +16,16 @@ public interface ProductRepository extends JpaRepository<Product, Long>, Product
 			"CASE WHEN COUNT(*) > 0 THEN true ELSE false END " +
 			"FROM " +
 			"Product p " +
-			"JOIN MerchantStore m ON m.id = ?2 " +
+			"JOIN MerchantStore m ON m.id = :store " +
 			"LEFT JOIN ProductVariant pv ON pv.product.id = p.id " +
-			"WHERE (pv.sku = ?1 OR p.sku = ?1)")
-	boolean existsBySku(String sku, Integer store);
+			"WHERE (pv.sku = :sku OR p.sku = :sku)")
+	boolean existsBySku(@Param("sku") String sku, @Param("store") Integer store);
 	
 	@Query(
 
-			value = "select p.PRODUCT_ID from {h-schema}PRODUCT p join {h-schema}MERCHANT_STORE m ON p.MERCHANT_ID = m.MERCHANT_ID left join {h-schema}PRODUCT_VARIANT i ON i.PRODUCT_ID = p.PRODUCT_ID where p.SKU=?1 or i.SKU=?1 and m.MERCHANT_ID=?2",
+			value = "select p.PRODUCT_ID from {h-schema}PRODUCT p join {h-schema}MERCHANT_STORE m ON p.MERCHANT_ID = m.MERCHANT_ID left join {h-schema}PRODUCT_VARIANT i ON i.PRODUCT_ID = p.PRODUCT_ID where p.SKU=:sku or i.SKU=:sku and m.MERCHANT_ID=:consultId",
 			nativeQuery = true
 	)
-	List<Object> findBySku(String sku, Integer consultId);
+	List<Object> findBySku(@Param("sku") String sku, @Param("consultId") Integer consultId);
 
 }
